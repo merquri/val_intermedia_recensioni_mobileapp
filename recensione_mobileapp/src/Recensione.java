ff4d2f5da8983fd47975e6424096a7641fd25bb8
merquri <80594881+merquri@users.noreply.github.com>
2025-02-11 09:46:18 +0100

    creazione recensione (1 fatto)

---
+++ recensione_mobileapp/src/Recensione.java
@@ -0,0 +1,58 @@
+import java.time.LocalDate;
+
+public class Recensione {
+
+    private LocalDate data;
+    private String nomeUtente;
+    private int numeroStelle;
+    private String testo;
+
+    //dichiaro valori min e max, finali perché sono quelli e non cambiano
+    private final int maxStelle = 5, minStelle = 1;
+
+    public Recensione(LocalDate data, String nomeUtente, int numeroStelle, String testo) {
+        this.data = LocalDate.now(); //la data della recensione viene presa sul momento
+        this.nomeUtente = nomeUtente;
+        this.setNumeroStelle(numeroStelle); //controllo del range nel setter
+        this.testo = testo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getNomeUtente() {
+        return nomeUtente;
+    }
+
+    public void setNomeUtente(String nomeUtente) {
+        this.nomeUtente = nomeUtente;
+    }
+
+    public int getNumeroStelle() {
+        return numeroStelle;
+    }
+
+    public void setNumeroStelle(int numeroStelle) {
+        //controllo se numeroStelle rientra nel range consentito
+        if (numeroStelle > maxStelle || numeroStelle < minStelle) {
+            throw new IllegalArgumentException(
+                    "Numero di stelle non valido, inserire un numero da " + minStelle + " a " + maxStelle + ".");
+        } else {
+            this.numeroStelle = numeroStelle;
+        }
+    }
+
+    public String getTesto() {
+        return testo;
+    }
+
+    public void setTesto(String testo) {
+        this.testo = testo;
+    }
+
+}
